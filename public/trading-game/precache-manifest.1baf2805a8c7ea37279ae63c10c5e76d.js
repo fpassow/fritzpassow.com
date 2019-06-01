fbf41aa266f1ad2c35ee7f20b5683be22c5d362a
fpassow <fpassow@yahoo.com>
2019-06-01 17:19:02 -0400

    Adding trading-game to website

---
+++ public/trading-game/precache-manifest.1baf2805a8c7ea37279ae63c10c5e76d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a281cc63d9459b08be14",
+    "url": "/trading-game/static/js/runtime~main.68757109.js"
+  },
+  {
+    "revision": "f552309410f2c9ccc0ee",
+    "url": "/trading-game/static/js/main.10c95a9c.chunk.js"
+  },
+  {
+    "revision": "611254752b169ab4b11a",
+    "url": "/trading-game/static/js/2.7e0ba1e2.chunk.js"
+  },
+  {
+    "revision": "fb7006bc4557cba22c28f35c4643664b",
+    "url": "/trading-game/index.html"
+  }
+];
